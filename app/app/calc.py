b9f58d01ae22cd37fd5f7bd2568099e4b0e86b2d
kranthu <g.kranthi2507@gmail.com>
2024-05-22 16:33:54 +0530

    tests and configure database initial steps

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y return the value"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract y from x and return the value"""
+    return x - y
