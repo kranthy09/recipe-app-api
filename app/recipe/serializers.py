80bb79e15cd2ef4940144dfe8c3b31bc4f85fab0
kranthu <g.kranthi2507@gmail.com>
2024-06-05 19:27:09 +0530

    Tags API update

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -15,7 +15,7 @@ class TagSerializer(serializers.ModelSerializer):
         read_only_fields = ["id"]
 
 
-class RecipeSerializer(serializers.Serializer):
+class RecipeSerializer(serializers.ModelSerializer):
     """Serialzer for recipes."""
 
     tags = TagSerializer(many=True, required=False)
@@ -27,28 +27,38 @@ class RecipeSerializer(serializers.Serializer):
             "id",
         ]
 
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         """create a recipe"""
         tags = validated_data.pop("tags", [])
         recipe = Recipe.objects.create(**validated_data)
-        auth_user = self.context["request"].user
-        print(tags)
-        for tag in tags:
-            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
-            recipe.tags.add(tag_obj)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
 
+    def update(self, instance, validated_data):
+        """update a recipe"""
+        tags = validated_data.pop("tags", [])
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
 
-class RecipeDetailSerializer(serializers.ModelSerializer):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+
+class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view"""
 
-    class Meta:
+    class Meta(RecipeSerializer.Meta):
         model = Recipe
-        fields = [
-            "id",
-            "title",
-            "time_minutes",
-            "price",
-            "link",
-            "description",
-        ]
+        fields = RecipeSerializer.Meta.fields + ["description"]
